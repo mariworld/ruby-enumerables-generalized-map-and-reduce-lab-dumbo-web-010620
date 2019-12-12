341266594e8f7b19dc5bc895390aba50d57d86ae
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-12 06:34:54 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
 # Your Code Here
+#write a generalized method for map and reduce
+#method should take an array and block
+def map(array)
+  new = []
+  index = 0
+  while index < array.length do
+    new.push(yield(array[index]))
+    index += 1
+  end
+  new
+end
+
+def reduce(array, starting_value = nil)
+  if starting_value
+    sum = starting_value
+    i = 0
+  else
+    sum = array[0]
+    i = 1
+  end
+  while i < array.length do
+    sum = yield(sum, array[i])
+    i += 1
+  end
+  sum
+end
